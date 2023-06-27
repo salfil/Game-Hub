f1cc4f2855e3c0c42def533e2c55a38a55192059
Salman Filli <salmanfilli123@hotmail.com>
2023-06-27 18:50:32 +0100

    Navbar and asside stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: '"nav" "main"', //for mobile no aside
+        lg: '"nav nav" "aside main"', //for large use aside
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="blue">
+          aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="gold">
+        main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
