c8f9f7fff19673aebee6ebdab150bfe641e9ce67
Salman Filli <salmanfilli123@hotmail.com>
2023-06-27 21:15:59 +0100

    custom hook to get games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,31 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { Grid, GridItem, Show, Text } from "@chakra-ui/react";
 import apiClient from "../services/api-client";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-  //stroing game objects
-  //state var for error messages too
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  //effect hook to send req to backend
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>("/games")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
-  });
-
+  const { games, error } = useGames();
   return (
     <>
       {error && <Text>{error}</Text>}
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react";
+import apiClient from "../services/api-client";
+
+
+interface Game {
+    id: number;
+    name: string;
+  }
+  
+  interface FetchGamesResponse {
+    count: number;
+    results: Game[];
+  }
+  
+
+
+
+const useGames=() => {
+    //stroing game objects
+  //state var for error messages too
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  //effect hook to send req to backend
+  useEffect(() => {
+    const controller = new AbortController();
+
+
+    apiClient
+      .get<FetchGamesResponse>("/games", {signal:controller.signal})
+      .then((res) => setGames(res.data.results))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message)
+    });
+    
+    return() => controller.abort();
+
+    }, []);
+
+  return {games, error};
+
+}
+
+
+export default useGames;
+
+
+
+
+
+
+
+
+
+
+
