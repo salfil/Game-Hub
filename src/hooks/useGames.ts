04ac4e4aed11d5c0f2f3edbc939bb915803b37a4
Salman Filli <salmanfilli123@hotmail.com>
2023-06-29 16:59:00 +0100

    loading skeletons.

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -30,25 +30,31 @@ const useGames=() => {
   //state var for error messages too
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   //effect hook to send req to backend
   useEffect(() => {
     const controller = new AbortController();
 
-
+    setLoading(true);
     apiClient
       .get<FetchGamesResponse>("/games", {signal:controller.signal})
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message)
+        setLoading(false);
     });
     
     return() => controller.abort();
 
     }, []);
 
-  return {games, error};
+  return {games, error, isLoading};
 
 }
 
