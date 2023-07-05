5cb380bf4707a050f2e0b3a40ef171c94ff5b5d6
Salman Filli <salmanfilli123@hotmail.com>
2023-07-05 17:48:33 +0100

    lookup hooks

--- src/hooks/UsePlatforms.ts
+++ src/hooks/UsePlatforms.ts
@@ -15,7 +15,7 @@ const usePlatforms = () => useQuery({
     queryKey:['platforms'],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, //24hr
-    initialData:{count: platforms.length, results: platforms}
+    initialData:{count: platforms.length, results: platforms, next:null}
 });
 
 export default usePlatforms;
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -17,7 +17,9 @@ const useGenres = () => useQuery({
     queryKey:['genres'],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000,  //24hr
-    initialData: {count:genres.length, results:genres}
+    initialData: {count:genres.length, 
+      results:genres
+    , next: null}
 })
 
 export default useGenres;
