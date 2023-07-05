0da5627bc9f11f5c601878a8e4c9153906d1f663
Salman Filli <salmanfilli123@hotmail.com>
2023-07-05 17:53:59 +0100

    simplify stale time

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,6 +3,7 @@ import { GameQuery } from "../App";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import APIClient from "../services/api-client";
 import { Platform } from "./UsePlatforms";
+import ms from "ms";
 
 const apiClient = new APIClient<Game>('/games');
 
@@ -35,7 +36,7 @@ const useGames=(gameQuery: GameQuery)  =>
         getNextPageParam: (lastPage, allPages) => {
           return lastPage.next ? allPages.length+1 : undefined;
         },
-        staleTime:24 * 60 * 60 * 1000 //24hr
+        staleTime: ms("24hr")//24hr
         
   });
   
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,8 +1,8 @@
 
 import {useQuery} from "@tanstack/react-query";
 import genres from "../data/genres";
+import ms from "ms";
 import APIClient from "../services/api-client";
-import { FetchResponse } from "../services/api-client";
 
 
 const apiClient = new APIClient<Genre>('/genres');
@@ -16,7 +16,7 @@ export interface Genre {
 const useGenres = () => useQuery({
     queryKey:['genres'],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000,  //24hr
+    staleTime: ms('24h'),  //24hr
     initialData: {count:genres.length, 
       results:genres
     , next: null}
