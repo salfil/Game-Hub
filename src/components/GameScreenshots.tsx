e06bd2488c95b5b836bd67aaeac9f7620fad8c09
Salman Filli <salmanfilli123@hotmail.com>
2023-07-08 18:40:52 +0100

    game screenshots

---
+++ src/components/GameScreenshots.tsx
@@ -0,0 +1,29 @@
+import useScreenshots from "../hooks/useScreenshots";
+import { SimpleGrid, Image } from "@chakra-ui/react";
+
+interface Props {
+  gameId: number;
+}
+
+const GameScreenshots = ({ gameId }: Props) => {
+  const { data, isLoading, error } = useScreenshots(gameId);
+
+  if (isLoading) return null;
+  if (error) throw error;
+
+  return (
+    <SimpleGrid
+      columns={{
+        base: 1,
+        md: 2,
+      }}
+      spacing={2}
+    >
+      {data?.results.map((file) => (
+        <Image key={file.id} src={file.image} />
+      ))}
+    </SimpleGrid>
+  );
+};
+
+export default GameScreenshots;
